77d73550273218435e6f9a1b665fd443c0f4b677
BehzadShayegh <behzad.shayegh@ut.ac.ir>
2021-07-11 19:17:58 +0430

    debug AOD: fully redirecting

--- src/abstract_object_decorator/abstract_object_decorator.py
+++ src/abstract_object_decorator/abstract_object_decorator.py
@@ -1,9 +1,17 @@
-from abc import ABC
-
-class AOD(ABC):
+class AOD:
     def __init__(self, obj):
         self.obj = obj
-        pass
 
-    def __getattr__(self, item):
-        return getattr(self.obj, item)
+    def __getattribute__(self, attribute):
+        if attribute in ['__dict__','__class__','obj'] :
+            return object.__getattribute__(self, attribute)
+        if attribute in self.__class__.__dict__:
+            return object.__getattribute__(self, attribute)
+        if attribute in self.__dict__:
+            return object.__getattribute__(self, attribute)
+        return self.obj.__getattribute__(attribute)
+
+    def __setattr__(self, attribute, value):
+        if attribute in ['obj'] :
+            return object.__setattr__(self, attribute, value)
+        return self.obj.__setattr__(attribute, value)
